148791e18b3300f5b19f931112b7a57e5a166acc
msilverman2428 <135058199+msilverman2428@users.noreply.github.com>
2023-06-01 10:03:23 -0400

    Add files via upload

---
+++ morning.py
@@ -0,0 +1,29 @@
+
+print("alarm!!")
+snooze=input("snooze? yes/no")
+while(snooze=="yes"):
+    print("try again five minutes later")
+    snooze=input("snooze? yes/no")
+if(snooze=="no"):
+    print("wake up!!!")
+
+shower=input("Shower? yes/no")
+if(shower=="yes"):
+    print("go in shower and wash hair")
+if(shower=="no"):
+   print("start getting ready") 
+
+print("start getting ready")
+cold=input("is it cold yes/no")
+if(cold=="yes"): 
+    print("wear a sweatshirt")
+if(cold=="no"):
+    print("dress for warm weather")
+
+tired=input("are you tired yes/no")
+if(tired=="yes"):
+    print("make a coffee then go to school")
+if(tired=="no"):
+    print("go to school")
+    
+print("have a good day !!")
